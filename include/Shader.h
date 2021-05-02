81513f95735a084ea3ba196914428eeccf6c3dce
Di Hu <dihu@promote.cache-dns.local>
2021-05-02 15:13:24 +0800

    init

---
+++ include/Shader.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "CommonHeader.h"
+class Shader{
+public:
+private:
+    std::string m_vertexPath;
+    std::string m_fragmentPath;
+};
---
+++ src/Shader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Di Hu on 5/2/21.
+//
+
+#include "../include/Shader.h"
