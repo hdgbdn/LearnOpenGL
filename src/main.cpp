7654336b01d3a103d104d06d052589a8a0b29e67
hudi <hdgbdn92@gmail.com>
2021-05-10 15:07:11 +0800

    finish cull so easy

--- src/main.cpp
+++ src/main.cpp
@@ -91,6 +91,8 @@ int main()
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_FRONT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     	if(needReloadShader)
