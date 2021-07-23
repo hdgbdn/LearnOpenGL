086c6f8abaadd2fa8af9dd573ca19388ea86bf85
hdgbdn <hdgbdn92@gmail.com>
2021-07-23 10:29:13 +0800

    update example: different colors

--- src/Examples/01_03_03_Different_Color/Different_Color.cpp
+++ src/Examples/01_03_03_Different_Color/Different_Color.cpp
@@ -1,117 +1,119 @@
-#include <stdio.h>
-#include <string>
-#include <filesystem>
 #include <iostream>
+#include "glad/glad.h"
 #include "Window.h"
+#include "Input.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+#include "Operation.h"
+#include "Shader.h"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include "GLFW/glfw3.h"
-#include "LambdaOp.h"
-#include "OpQueue.h"
-#include "Shader.h"
 
 using namespace std;
-using namespace Hdgbdn;
+using namespace hdgbdn;
 namespace fs = std::filesystem;
 
 // settings
 const unsigned int SCR_WIDTH = 1366;
 const unsigned int SCR_HEIGHT = 768;
-const string APP_NAME = "Draw 2 Triangles";
+const string APP_NAME = "Different colors";
+
+const fs::path shader_path = fs::current_path().parent_path() / "Shaders";
 
 int main(int, char**)
 {
 
 	cout << APP_NAME << endl;
 	Window window(SCR_WIDTH, SCR_HEIGHT, APP_NAME);
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	(void)io;
+	Input input(window);
 
-	ImGui::StyleColorsDark();
-    
-	ImGui_ImplGlfw_InitForOpenGL(window.GetWindow(), true);
-	ImGui_ImplOpenGL3_Init("#version 130");
+    VAO vao1{ 3 };
+	VBO vbo1{
+		0.5f,  0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+	};
+	VAO::Bind(vao1);
+	vbo1.BufferData();
+	VAO::SetAttrPointer(vao1);
+	VAO::UnBind();
 
-    float vertices_1[] = {
-     0.5f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
-    };
+	VAO vao2{ 3 };
+	VBO vbo2{
+		-0.5f, -0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f,
+		0.5f,  0.5f, 0.0f,
+	};
+	VAO::Bind(vao2);
+	vbo2.BufferData();
+	VAO::SetAttrPointer(vao2);
+	VAO::UnBind();
 	
-    float vertices_2[] = {
-    -0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f,
-     0.5f,  0.5f, 0.0f,
-    };
-
-    // 1.generate buffer and data object
-    unsigned int VAO_1, VBO_1;
-    glGenVertexArrays(1, &VAO_1);
-    glGenBuffers(1, &VBO_1);
-	// 1.1 bind VAO at first
-    glBindVertexArray(VAO_1);
-	// 2.bind our VBO to array buffer, and copy vertices into a vertex buffer for OpenGL
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_1);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
-	// 4.set the vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    unsigned int VAO_2, VBO_2;
-    glGenVertexArrays(1, &VAO_2);
-    glGenBuffers(1, &VBO_2);
-    glBindVertexArray(VAO_2);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_2);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_2), vertices_2, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-	// unbind buffers and VAO
-    glBindVertexArray(0);
-
-    const fs::path shader_path = fs::current_path().parent_path() / "Shaders";
     Shader shader((shader_path / "vertex.vs").string(), (shader_path / "fragment.fs").string());
-    Shader shader_yellow((shader_path / "vertex.vs").string(), (shader_path / "fragment_yellow.fs").string());
+    Shader shader_2((shader_path / "vertex.vs").string(), (shader_path / "fragment_yellow.fs").string());
 
-	auto renderOp = new LambdaOp([&]()
+	glm::vec3 color1(0.f, 1.f, 0.f);
+	glm::vec3 color2(1.f, 0.f, 0.f);
+	
+	window.PushPreRenderOperation(Operation([&window]()
+	{
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO();
+		(void)io;
+
+		ImGui::StyleColorsDark();
+
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init("#version 130");
+	}));
+	
+	window.PushRenderOperation(Operation([&]
 		{
-            glfwPollEvents();
+			glfwPollEvents();
+			input.Update();
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			shader.Use();
+			shader.set("color", color1);
+			VAO::Bind(vao1);
+			vbo1.Draw();
+
+			shader_2.Use();
+			shader_2.set("color", color2);
+			VAO::Bind(vao2);
+			vbo2.Draw();
+
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
-
-			// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 			{
-				static float f = 0.0f;
-				static int counter = 0;
-
-				ImGui::Begin(APP_NAME.c_str()); // Create a window called "Hello, world!" and append into it.
-
-				
+				ImGui::Begin(APP_NAME.c_str());
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::ColorEdit3("color1", (float*)&color1);
+				ImGui::ColorEdit3("color2", (float*)&color2);
 				ImGui::End();
 			}
 
-			// Rendering
-            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT);
-            shader.Use();
-            glBindVertexArray(VAO_1);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-            shader_yellow.Use();
-            glBindVertexArray(VAO_2);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-            glBindVertexArray(0);
-
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-			glfwSwapBuffers(window.GetWindow());
-		});
-    auto opQueue = new OpQueue;
-    (*opQueue) << renderOp;
-    window.Run(opQueue);
+		
+			glfwSwapBuffers(window);
+		}));
+	window.PushPostRenderOperation(Operation([]()
+		{
+			glfwTerminate();
+		}));
+
+	input.SetKeyDownCb(GLFW_KEY_ESCAPE, Operation([&window]()
+		{
+			window.Close();
+		}));
+
+	Window::StartRenderLoop(window);
 
 	return 0;
 }
