6168b368a01a9e8a948c047f167a5adb17871b90
hdgbdn <hdgbdn92@gmail.com>
2021-07-27 14:57:58 +0800

    OOP camera

--- include/LOPGL/Camera.h
+++ include/LOPGL/Camera.h
@@ -11,6 +11,13 @@ enum Camera_Movement
     RIGHT
 };
 
+enum Camera_Control_Mode
+{
+	NONE,
+	FREE,
+	ARCBALL,
+};
+
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
 const float SPEED = 2.5f;
@@ -20,6 +27,33 @@ const float ZOOM = 45.0f;
 class Camera
 {
 public:
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ,
+        float yaw, float pitch);
+
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+        float yaw = YAW, float pitch = PITCH);
+	
+
+    glm::mat4 GetViewMatrix() const;
+
+    void SetControlMode(Camera_Control_Mode);
+
+    void Move(Camera_Movement direction, float deltaTime);
+    void RotateByMouse(float xOffset, float yOffset, GLboolean constrainPitch = false);
+    void ProcessMouseScroll(float yOffset);
+
+    glm::vec3 GetPosition() const;
+    glm::vec3 GetFront() const;
+    glm::vec3 GetUp() const;
+    glm::vec3 GetRight() const;
+    glm::vec3 GetWorldUp() const;
+    float GetYaw() const;
+    float GetPitch() const;
+    float GetZoom() const;
+
+private:
+    Camera_Control_Mode ctrlMode;
     glm::vec3 Position;
     glm::vec3 Front;
     glm::vec3 Up;
@@ -32,39 +66,9 @@ public:
     float MovementSpeed;
     float MouseSensitivity;
     float Zoom;
-
+	
     bool ViewMode;
-
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
-                   glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
-                   float yaw = YAW, float pitch = PITCH)
-            : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED),
-              MouseSensitivity(SENSITIVITY), Zoom(ZOOM), ViewMode(false)
-    {
-        Position = position;
-        WorldUp = up;
-        Yaw = yaw;
-        Pitch = pitch;
-        updateCameraVectors();
-    }
-    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ,
-                   float yaw, float pitch)
-            : MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM), ViewMode(false)
-    {
-        Position = glm::vec3(posX, posY, posZ);
-        WorldUp = glm::vec3(upX, upY, upZ);
-        Yaw = yaw;
-        Pitch = pitch;
-        updateCameraVectors();
-    }
-
-    glm::mat4 GetViewMatrix();
-
-    void Move(Camera_Movement direction, float deltaTime);
-    void RotateByMouse(float xOffset, float yOffset, GLboolean constrainPitch = false);
-    void ProcessMouseScroll(float yOffset);
-
-private:
+	
     void updateCameraVectors();
 };
 
--- src/LOPGL/Camera.cpp
+++ src/LOPGL/Camera.cpp
@@ -1,45 +1,104 @@
 #include "Camera.h"
 
-glm::mat4 Camera::GetViewMatrix()
+Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ,
+    float yaw, float pitch)
+    : Position(posX, posY, posZ), WorldUp(upX, upY, upZ), Yaw(yaw), Pitch(pitch),
+    Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM), ctrlMode(Camera_Control_Mode::NONE)
+{
+    updateCameraVectors();
+}
+
+Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
+    : Camera(position.x, position.y, position.z, up.x, up.y, up.z, yaw, pitch) {}
+
+glm::vec3 Camera::GetPosition() const
+{
+    return Position;
+}
+
+glm::vec3 Camera::GetFront() const
+{
+    return Front;
+}
+
+glm::vec3 Camera::GetUp() const
+{
+    return Up;
+}
+
+glm::vec3 Camera::GetRight() const
+{
+    return Right;
+}
+
+glm::vec3 Camera::GetWorldUp() const
+{
+    return WorldUp;
+}
+
+float Camera::GetYaw() const
+{
+    return Yaw;
+}
+
+float Camera::GetPitch() const
+{
+    return Pitch;
+}
+
+float Camera::GetZoom() const
+{
+    return Zoom;
+}
+
+glm::mat4 Camera::GetViewMatrix() const
 {
     return glm::lookAt(Position, Position + Front, Up);
 }
 
+void Camera::SetControlMode(Camera_Control_Mode mode)
+{
+    ctrlMode = mode;
+}
+
+
 void Camera::Move(Camera_Movement direction, float deltaTime)
 {
-    float velocity = MovementSpeed * deltaTime;
+    float distance = MovementSpeed * deltaTime;
     if (direction == FORWARD)
-        Position += Front * velocity;
+        Position += Front * distance;
     if (direction == BACKWARD)
-        Position -= Front * velocity;
+        Position -= Front * distance;
     if (direction == LEFT)
-        Position -= Right * velocity;
+        Position -= Right * distance;
     if (direction == RIGHT)
-        Position += Right * velocity;
+        Position += Right * distance;
 }
 
 void Camera::RotateByMouse(float xoffset, float yoffset, GLboolean constrainPitch)
 {
-    if(!ViewMode) return;
-    xoffset *= MouseSensitivity;
-    yoffset *= MouseSensitivity;
-
-    Yaw += xoffset;
-    Pitch -= yoffset;
-
-    if (constrainPitch)
+    if(ctrlMode == Camera_Control_Mode::FREE)
     {
-        if (Pitch > 89.0f)
-            Pitch = 89.0f;
-        if (Pitch < -89.0f)
-            Pitch = -89.0f;
+        xoffset *= MouseSensitivity;
+        yoffset *= MouseSensitivity;
+
+        Yaw += xoffset;
+        Pitch -= yoffset;
+
+        if (constrainPitch)
+        {
+            if (Pitch > 89.0f)
+                Pitch = 89.0f;
+            if (Pitch < -89.0f)
+                Pitch = -89.0f;
+        }
+        updateCameraVectors();
     }
-    updateCameraVectors();
 }
 
 void Camera::ProcessMouseScroll(float yoffset)
 {
-    Zoom -= (float)yoffset;
+    Zoom -= yoffset;
     if (Zoom < 1.0f)
         Zoom = 1.0f;
     if (Zoom > 45.0f)
