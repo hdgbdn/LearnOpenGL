fdb7585d65138404fa344fc0d56f929e83abc108
hdgbdn <hdgbdn92@gmail.com>
2021-07-27 14:58:28 +0800

    add release call back

--- include/Utility/Input.h
+++ include/Utility/Input.h
@@ -6,13 +6,17 @@
 #include "GLFW/glfw3.h"
 #include "Operation.h"
 #include "Window.h"
+#include "glm/glm.hpp"
 
 namespace hdgbdn
 {
 	class Window;
 	class Input
 	{
+		using CursorPosFunc = std::function<void(double, double, double, double)>;
 		friend void glfwKeyCbHelper(GLFWwindow*, int, int, int, int);
+		friend void glfwCursorPosCbHelper(GLFWwindow*, double, double);
+		friend void glfwMouseButtonCbHelper(GLFWwindow*, int, int, int);
 	public:
 		Input(Window&);
 		~Input();
@@ -21,18 +25,36 @@ namespace hdgbdn
 		Input& operator=(const Input&) = delete;
 		
 		void SetKeyDownCb(int key, Operation);
+		void SetKeyReleaseCb(int key, Operation);
 		void RemoveKeyDownCb(int key);
+		void SetCursorPosCb(CursorPosFunc&&);
+		void SetCursorPosCb(const CursorPosFunc&);
 		void Update();
 		double GetDelta() const;
+		glm::vec2 GetCursorPos();
+		glm::vec2 GetCursorDelta();
+
+		void ShowCursor();
+		void HideCursor();
+		void DisableCursor();
 	private:
 		GLFWwindow* win;
 		double lastFrame;
 		double delta;
-		const int keyCount = 512;
+		const int keyCount;
+		double lastCursorX;
+		double lastCursorY;
+		double cursorDeltaX;
+		double cursorDeltaY;
+		bool cursorChanged;
+		
 		std::unordered_map<int, Operation> keyDownOp;
-		std::unordered_map<int, Operation> keyPressOp;
+		std::unordered_map<int, Operation> keyReleaseOp;
 		std::vector<bool> keyDown;
+		std::vector<bool> keyRelease;
+		std::vector<CursorPosFunc> vecCursorPosCb;
 		bool IsKeyDown(int);
+		bool IsKeyRelease(int);
 		static std::unordered_map<GLFWwindow*, Input*> winInputMap;
 	};
 
--- src/Utility/Input.cpp
+++ src/Utility/Input.cpp
@@ -21,11 +21,55 @@ void hdgbdn::glfwKeyCbHelper(GLFWwindow* w, int key, int scancode, int action, i
 	}
 }
 
-Input::Input(Window& win):win(win.get()) ,lastFrame(0.0), delta(0.0), keyDownOp(), keyPressOp(), keyDown(keyCount, false)
+void hdgbdn::glfwCursorPosCbHelper(GLFWwindow* w, double posX, double posY)
+{
+	Input* pInput = Input::winInputMap[w];
+	if(pInput != nullptr)
+	{
+		pInput->cursorChanged = true;
+		const double lastX = pInput->lastCursorX;
+		const double lastY = pInput->lastCursorY;
+		pInput->lastCursorX = posX;
+		pInput->lastCursorY = posY;
+		pInput->cursorDeltaX = posX - lastX;
+		pInput->cursorDeltaY = posY - lastY;
+	}
+}
+
+void hdgbdn::glfwMouseButtonCbHelper(GLFWwindow* w, int button, int action, int mod)
+{
+	Input* pInput = Input::winInputMap[w];
+	if (pInput != nullptr)
+	{
+		if (button >= 0 && button < pInput->keyCount)
+		{
+			if (action == GLFW_PRESS)
+				pInput->keyDown[button] = true;
+			if (action == GLFW_RELEASE)
+			{
+				for(int i = 0; i < pInput->keyRelease.size(); ++i) pInput->keyRelease[button] = false;
+				// only previous is down and current is released, can trigger release
+				if (pInput->keyDown[button] == true)
+					pInput->keyRelease[button] = true;
+				else
+					pInput->keyRelease[button] = false;
+				
+				pInput->keyDown[button] = false;
+			}
+		}
+	}
+}
+
+Input::Input(Window& win) :
+	win(win.get()), lastFrame(0.0), delta(0.0), keyCount(512),
+	lastCursorX(), lastCursorY(), cursorDeltaX(), cursorDeltaY(), cursorChanged(false),
+	keyDownOp(), keyReleaseOp(), keyDown(keyCount, false), keyRelease(keyCount, false), vecCursorPosCb()
 {
 	if (winInputMap.find(win) != winInputMap.end()) throw(invalid_argument("already have a Input with this window"));
 	winInputMap.insert({win.get(), this});
 	glfwSetKeyCallback(win, glfwKeyCbHelper);
+	glfwSetCursorPosCallback(win, glfwCursorPosCbHelper);
+	glfwSetMouseButtonCallback(win, glfwMouseButtonCbHelper);
 }
 
 double Input::GetDelta() const
@@ -33,20 +77,85 @@ double Input::GetDelta() const
 	return delta;
 }
 
+glm::vec2 Input::GetCursorPos()
+{
+	return glm::vec2(lastCursorX, lastCursorY);
+}
+
+
+glm::vec2 Input::GetCursorDelta()
+{
+	return glm::vec2(cursorDeltaX, cursorDeltaY);
+}
+
+
 void Input::Update()
 {
 	double newFrame = glfwGetTime();
 	delta = newFrame - lastFrame;
+	lastFrame = newFrame;
+	
+	// invoke key call back every frame
 	for(int i = 0; i < keyDown.size(); ++i)
 	{
 		if (IsKeyDown(i) && (keyDownOp.find(i) != keyDownOp.end())) keyDownOp[i]();
+		if (IsKeyRelease(i) && (keyReleaseOp.find(i) != keyReleaseOp.end())) 
+		{
+			keyRelease[i] = false;
+			keyReleaseOp[i]();
+		}
 	}
+	
+	// only invoke cursor position call back when position changed
+	if(cursorChanged)
+	{
+		cursorChanged = false;
+		for(auto cb : vecCursorPosCb)
+		{
+			cb(lastCursorX, lastCursorY, cursorDeltaX, cursorDeltaY);
+		}
+	}
+	
 }
+
+void Input::ShowCursor()
+{
+	glfwSetInputMode(win, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+}
+
+void Input::HideCursor()
+{
+	glfwSetInputMode(win, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+}
+
+
+void Input::DisableCursor()
+{
+	glfwSetInputMode(win, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
 
 void Input::SetKeyDownCb(int key, Operation op)
 {
 	keyDownOp[key] = op;
 }
+
+void Input::SetKeyReleaseCb(int key, Operation op)
+{
+	keyReleaseOp[key] = op;
+}
+
+
+void Input::SetCursorPosCb(const CursorPosFunc& func)
+{
+	vecCursorPosCb.push_back(func);
+}
+
+void Input::SetCursorPosCb(CursorPosFunc&& func)
+{
+	vecCursorPosCb.push_back(std::move(func));
+}
+
 
 void Input::RemoveKeyDownCb(int key)
 {
@@ -56,6 +165,11 @@ void Input::RemoveKeyDownCb(int key)
 bool Input::IsKeyDown(int key)
 {
 	return keyDown[key];
+}
+
+bool Input::IsKeyRelease(int key)
+{
+	return keyRelease[key];
 }
 
 Input::~Input()
